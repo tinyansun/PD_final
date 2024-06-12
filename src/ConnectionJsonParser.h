6b9964c8fe92910bb65ed03a49ac2d1c524b3113
wenwen7887 <140996191+wenwen7887@users.noreply.github.com>
2024-06-12 15:11:10 +0800

    Update ConnectionJsonParser.h
    Add routing segment 2D list to net info

--- src/ConnectionJsonParser.h
+++ src/ConnectionJsonParser.h
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <nlohmann/json.hpp>
+#include "json.hpp"
 
 using namespace std;
 
@@ -15,6 +15,27 @@ public:
         int numTracks;
         pair<double, double> txCoord;
         vector<pair<double, double>> rxCoord;
+        vector<vector<Grid> > routingSegment;
+        double CalHPWL() {
+            double min_x = -INT_MAX;
+            double max_x = INT_MAX;
+            double min_y = -INT_MAX;
+            double max_y = INT_MAX;
+            for (int i=0; i<routingSegment.size(); i++) {
+                vector<Grid> twoPinSegment = routingSegment[i];
+                for (int j=0; twoPinSegment.size(); j++) {
+                    Grid grid = twoPinSegment[j];
+                    double x = grid.get_x();
+                    double y = grid.get_y();
+                    min_x = min(min_x, x);
+                    max_x = max(max_x, x);
+                    min_y = min(min_y, y);
+                    max_y = max(max_y, y);
+                }
+            }
+
+            return (max_x - min_x) + (max_y - min_y);
+        }   
 
         NetInfo(int id, const string& tx, const vector<string>& rx, int num,
                 pair<double, double> txc, const vector<pair<double, double>>& rxc)
@@ -24,6 +45,7 @@ public:
 private:
     string filePath;
     vector<NetInfo> nets;
+    int maxTrack = 0;
 
 public:
     ConnectionJsonParser(const string& path) : filePath(path) {}
@@ -39,6 +61,9 @@ public:
         jsonFile >> j;
 
         for (const auto& net : j) {
+            if(net["NUM"] > maxTrack) {
+                maxTrack = net["NUM"];
+            }
             vector<string> rx = net["RX"];
             vector<pair<double, double>> rxCoord;
             for (const auto& coord : net["RX_COORD"]) {
@@ -61,5 +86,9 @@ public:
 
     const vector<NetInfo>& getNets() const {
         return nets;
+    }
+
+    const int& getmaxTrack() const {
+        return maxTrack;
     }
 };
